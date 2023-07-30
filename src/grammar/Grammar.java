11fa5523176c3597dcd2420095d678ad773e608d
孟寅磊 <11616493+meng-yinlei@user.noreply.gitee.com>
2023-07-30 11:03:03 +0800

    add detailed grammar definition

--- src/grammar/Grammar.java
+++ src/grammar/Grammar.java
@@ -1,3 +1,51 @@
 package grammar;
 
 // Grammar
+//
+// program -> block
+// block   -> { decls stmts }
+// decls   -> decls decl
+//          | ɛ
+// decl    -> type id ;
+// type    -> type [ num ]
+//          | basic
+// stmts   -> stmts stmt
+//          | ɛ
+//
+// stmt -> loc = bool ;
+//       | if ( bool ) stmt
+//       | if ( bool ) stmt else stmt
+//       | while ( bool ) stmt
+//       | do stmt while ( bool ) ;
+//       | break ;
+//       | block
+// loc  -> loc [ bool ]
+//       | id
+//
+// bool     -> bool || join
+//           | join
+// join     -> join && equality
+//           | equality
+// equality -> equality == rel
+//           | equality != rel
+//           | rel
+// rel      -> expr < expr
+//           | expr <= expr
+//           | expr > expr
+//           | expr >= expr
+//           | expr
+// expr     -> expr + term
+//           | expr - term
+//           | term
+// term     -> term * unary
+//           | term / unary
+//           | unary
+// unary    -> ! unary
+//           | - unary
+//           | factor
+// factor   -> ( bool )
+//           | loc
+//           | num
+//           | real
+//           | true
+//           | false
